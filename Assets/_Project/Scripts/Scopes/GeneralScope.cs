2a3783c833eba3491599d6440ae937cf4bf0b532
gamedevdeniz <mdeniz.sener@gmail.com>
2023-04-17 14:42:45 +0300

    Update

--- Assets/_Project/Scripts/Scopes/GeneralScope.cs
+++ Assets/_Project/Scripts/Scopes/GeneralScope.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VContainer.Unity;
 
-public class GeneralScope : MonoBehaviour
+namespace Scopes
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GeneralScope : LifetimeScope
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
